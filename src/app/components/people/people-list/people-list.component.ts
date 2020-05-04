cad2a96ad6aec44eb3c042e5b3e25d1fd2031405
kaandrade3 <63884782+kaandrade3@users.noreply.github.com>
2020-05-04 07:39:19 -0500

    Update people-list.component.ts

--- src/app/components/people/people-list/people-list.component.ts
+++ src/app/components/people/people-list/people-list.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { CharacterService } from '../character.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
-  selector: 'app-people-list',
-  templateUrl: './people-list.component.html',
-  styleUrls: ['./people-list.component.scss']
+  selector: 'app-character-list',
+  templateUrl: './character-list.component.html',
+  styleUrls: ['./character-list.component.scss']
 })
-export class PeopleListComponent implements OnInit {
+export class CharacterListComponent implements OnInit {
 
-  constructor() { }
+  @Output() selectedCharacter: EventEmitter<string> = new EventEmitter<string>();
+  $charactersJSON: Observable<{name: string, url: string}>;
+  activeChar: string;
 
-  ngOnInit(): void {
+  constructor(private cs: CharacterService) { }
+
+  ngOnInit() {
+    this.$charactersJSON = this.cs.getCharacters();
+  }
+
+  handleCharSelection(character: {name: string; url: string}) {
+    this.activeChar = character.name;
+    this.selectedCharacter.emit(character.url);
   }
 
 }
