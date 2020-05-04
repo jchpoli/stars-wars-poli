41407398111752d5b0a0049cff0a3cd1cbf0f6f4
albustamante2 <albustamante2@poligran.edu.co>
2020-05-04 11:10:10 -0500

    "Modulo Vehiculos"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { SliderModule } from 'angular-image-slider';
 import { NgxSpinnerModule } from "ngx-spinner";
 import {FormsModule} from '@angular/forms';
 import {FilterPipe} from './components/films/film/filterFilms.pipe';
+import {FilterPipeVehicles} from './components/vehicles/vehicle/filterVehicle.pipe';
 import { DatePipe } from '@angular/common';
 
 @NgModule({
@@ -42,7 +43,8 @@ import { DatePipe } from '@angular/common';
           PeopleListComponent,
           FilmListComponent,
           HomeComponent,
-          FilterPipe
+          FilterPipe,
+          FilterPipeVehicles
      ],
      imports: [
           BrowserModule,
--- src/app/components/vehicles/vehicle/vehicle.component.ts
+++ src/app/components/vehicles/vehicle/vehicle.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
+import { IVehicles } from 'src/app/models/IVehicles';
 @Component({
   selector: 'app-vehicle',
   templateUrl: './vehicle.component.html',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehicleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
-  ngOnInit(): void {
+  vehiculos;
+  selectVehiculo: IVehicles;
+  apiResponse;
+  searchNombre;
+  searchModelo;
+  searchPasajeros;
+  imagenes = ["../../assets/images/Vehiculo1.jpg","../../assets/images/Vehiculo2.jpg","../../assets/images/Vehiculo3.jpg","../../assets/images/Vehiculo4.jpg","../../assets/images/Vehiculo5.jpg","../../assets/images/Vehiculo6.jpg","../../assets/images/Vehiculo7.jpg","../../assets/images/Vehiculo8.jpg","../../assets/images/Vehiculo9.jpg","../../assets/images/Vehiculo10.jpg"];
+
+  async ngOnInit() {
+    this.apiResponse = await this.httpClient.get('https://swapi.dev/api/vehicles/').toPromise();
+    this.vehiculos=this.apiResponse.results;
+    for (let i = 0; i < this.vehiculos.length; i++) {
+      this.vehiculos[i].img=this.imagenes[i];
+
+    }
+    console.log(this.vehiculos);
+
   }
+
+  detalleVehiculo(vehiculoClick : IVehicles){
+    this.selectVehiculo= vehiculoClick;
+  }
+
+
+
 
 }
